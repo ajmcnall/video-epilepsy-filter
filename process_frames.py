ae54290a699bfc84b193e66c57ff218e6a399161
Ethan Hong <yihanh@umich.edu>
2018-03-04 15:37:12 -0500

    Rearranging when to write out the video so you can modify multiple frames at once

--- process_frames.py
+++ process_frames.py
@@ -7,19 +7,26 @@ cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
+frames = []
+
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
-        frame = cv2.flip(frame,0)
+        frames.append(frame)
+        
+        # frame = cv2.flip(frame,0)
 
-        # write the flipped frame
-        out.write(frame)
+        # # write the flipped frame
+        # out.write(frame)
 
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        # cv2.imshow('frame',frame)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
     else:
         break
+
+for frame in frames:
+    out.write(cv2.flip(frame, 0))
 
 # Release everything if job is finished
 cap.release()
