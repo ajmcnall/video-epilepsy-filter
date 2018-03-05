2ea2e6d7e49227f89d670e0feb1996e29f83f0dd
Ethan Hong <yihanh@umich.edu>
2018-03-05 14:21:42 -0500

    algorithm should be in there

--- video_analysis/process_frames.py
+++ video_analysis/process_frames.py
@@ -25,7 +25,6 @@ while(cap.isOpened() and len(frames) <= 60):
         break
         
     frames.append(Frame(frame))
-    print len(frames)
     
     # frame = cv2.flip(frame,0)
 
@@ -36,8 +35,42 @@ while(cap.isOpened() and len(frames) <= 60):
     # if cv2.waitKey(1) & 0xFF == ord('q'):
     #     break
 
-for frame in frames:
-    out.write(cv2.flip(frame.raw_array, 0))
+# for frame in frames:
+#     out.write(cv2.flip(frame.raw_array, 0))
+
+general_idxs = []
+red_idxs = []
+
+# Start on second frame so you can compare it to first frame
+for idx, current_frame in enumerate(frames[1:]):
+    previous_frame = frames[idx - 1]
+
+    darker_L = 0
+    lighter_L = 0
+    if current_frame.L < previous_frame.L:
+        darker_L = current_frame.L
+        lighter_L = previous_frame.L
+    else:
+        darker_L = previous_frame.L
+        lighter_L = current_frame.L
+
+    # general oppositing transition formula
+    if darker_L < 0.80 and lighter_L - darker_L > lighter_L * 0.10:
+        # opposing transition found
+        general_idxs.append(idx)
+
+    # red opposing transition formula
+    # it's possible previous threshold isn't needed - fixme - see algorithm again
+    previous_threshold = previous_frame.R / (previous_frame.R + previous_frame.G + previous_frame.B)
+    current_threshold = current_frame.R / (current_frame.R + current_frame.G + current_frame.B)
+
+    previous_delta = (previous_frame.R - previous_frame.G - previous_frame.B) * 320
+    current_delta = (current_frame.R - current_frame.G - current_frame.B) * 320
+
+    if previous_threshold > 0.80 and current_threshold > 0.80 and \
+        current_delta - previous_delta > 0:
+        # red transition found
+        red_idxs.append(idx)
 
 # Release everything if job is finished
 cap.release()
