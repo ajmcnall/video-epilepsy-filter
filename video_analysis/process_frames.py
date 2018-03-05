57080101577f9936c456eb1384c7d4d69133f3dd
Ethan Hong <yihanh@umich.edu>
2018-03-05 14:40:00 -0500

    this doesn't work yet, but has important comments on how to make it working later - algorithm should be per pixel not per frame

--- video_analysis/process_frames.py
+++ video_analysis/process_frames.py
@@ -28,17 +28,21 @@ red_idxs = []
 for idx, current_frame in enumerate(frames[1:]):
     previous_frame = frames[idx - 1]
 
-    darker_L = 0
-    lighter_L = 0
-    if current_frame.L < previous_frame.L:
-        darker_L = current_frame.L
-        lighter_L = previous_frame.L
-    else:
-        darker_L = previous_frame.L
-        lighter_L = current_frame.L
+    # darker_L = 0
+    # lighter_L = 0
+    # if current_frame.L < previous_frame.L:
+    #     darker_L = current_frame.L
+    #     lighter_L = previous_frame.L
+    # else:
+    #     darker_L = previous_frame.L
+    #     lighter_L = current_frame.L
 
     # general oppositing transition formula
-    if darker_L < 0.80 and lighter_L - darker_L > lighter_L * 0.10:
+    # fixme - this following part should be in a loop
+    # and pixels should be considered epileptic individually before seeing if
+    # total # epileptic pixels > all pixels in the frame/36
+    if (current_frame.L < 0.8 or previous_frame < 0.8) and \
+       current_frame.L - previous_frame.L > lighter_L * 0.10:
         # opposing transition found
         general_idxs.append(idx)
 
@@ -50,10 +54,15 @@ for idx, current_frame in enumerate(frames[1:]):
     previous_delta = (previous_frame.R - previous_frame.G - previous_frame.B) * 320
     current_delta = (current_frame.R - current_frame.G - current_frame.B) * 320
 
+    # fixme - do this following part pixel by pixel, 
+    # then see if total number of detected pixels is greater than 1/36 of total pixels
     if previous_threshold > 0.80 and current_threshold > 0.80 and \
         current_delta - previous_delta > 0:
         # red transition found
         red_idxs.append(idx)
+
+# print (general_idxs)
+# print (red_idxs)
 
 # Release everything if job is finished
 cap.release()
