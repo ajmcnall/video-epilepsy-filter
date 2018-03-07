c2708dd96b4214f337e7036f103ef5f70a01f13b
Ethan Hong <yihanh@umich.edu>
2018-03-07 16:27:13 -0500

    avoid divide by 0 error

--- video_analysis/process_frames.py
+++ video_analysis/process_frames.py
@@ -2,18 +2,85 @@ import numpy as np
 import cv2
 from frame import Frame
 
+
+def general_transition(previous_frame, current_frame):
+    height = previous_frame.raw_array.shape[0]
+    width = previous_frame.raw_array.shape[1]
+
+    # fixme - not sure what maximum luminance means
+    # # maximum L among all pixels in both frames
+    # max_L = 0.0
+
+    general_count = 0
+    for x in range(height):
+        for y in range(width):
+            darker_L = 0
+            lighter_L = 0
+            if current_frame.L[x][y] < previous_frame.L[x][y]:
+                darker_L = current_frame.L[x][y]
+                lighter_L = previous_frame.L[x][y]
+            else:
+                darker_L = previous_frame.L[x][y]
+                lighter_L = current_frame.L[x][y]
+
+            if darker_L < 0.8 and lighter_L - darker_L > lighter_L * 0.1:
+                general_count = general_count + 1
+    if general_count > previous_frame.raw_array.size / 36:
+        # detected
+        return True
+    return False
+
+
+def red_transition(previous_frame, current_frame):
+    height = previous_frame.raw_array.shape[0]
+    width = previous_frame.raw_array.shape[1]
+
+    red_count = 0
+    for x in range(height):
+        for y in range(width):
+            # red opposing transition formula
+            # it's possible previous threshold isn't needed - fixme - see algorithm again
+
+            previous_rgb_sum = previous_frame.R[x][y] + previous_frame.G[x][y] + previous_frame.B[x][y]
+            # prevent divide by zero error
+            if previous_rgb_sum == 0:
+                previous_rgb_sum = 1
+            previous_threshold = previous_frame.R[x][y] / previous_rgb_sum
+
+            current_rgb_sum = current_frame.R[x][y] + current_frame.G[x][y] + current_frame.B[x][y]
+            if current_rgb_sum == 0:
+                current_rgb_sum = 1
+            current_threshold = current_frame.R[x][y] / current_rgb_sum
+
+            previous_delta = (previous_frame.R[x][y] - previous_frame.G[x][y] - previous_frame.B[x][y]) * 320
+            current_delta = (current_frame.R[x][y] - current_frame.G[x][y] - current_frame.B[x][y]) * 320
+
+            # fixme - do this following part pixel by pixel, 
+            # then see if total number of detected pixels is greater than 1/36 of total pixels
+            if previous_threshold > 0.80 and current_threshold > 0.80 and \
+                current_delta - previous_delta > 0:
+                # red transition found
+                red_count = red_count + 1
+    
+    if red_count > previous_frame.raw_array.size / 36:
+        return True
+    return False
+
+
+def process_idxs(idxs):
+    print(len(idxs))
+    idx_iter = iter(idxs)
+    pass
+
+
 # string is the name of the video we want to analyze
-cap = cv2.VideoCapture('aotl')
+cap = cv2.VideoCapture('test_video.avi')
 
 frames = []
 
-# fixme - remove the 60 limit later
-while(cap.isOpened() and len(frames) <= 60):
+# fixme - remove the 60 limit later - and len(frames) <= 60
+while(cap.isOpened()):
     ret, frame = cap.read()
-    print(frame.shape)
-    print(frame[0][0])
-    print(frame[0][0][2])
-    exit(1)
 
     # stop when no more frames to read
     if ret == False:
@@ -56,64 +123,3 @@ process_idxs(red_idxs)
 cap.release()
 cv2.destroyAllWindows()
 
-
-def general_transition(previous_frame, current_frame):
-    height = previous_frame.raw_array.shape[0]
-    width = previous_frame.raw_array.shape[1]
-
-    # fixme - not sure what maximum luminance means
-    # # maximum L among all pixels in both frames
-    # max_L = 0.0
-
-    general_count = 0
-    for x in range(height):
-        for y in range(width):
-            darker_L = 0
-            lighter_L = 0
-            if current_frame.L[x][y] < previous_frame.L[x][y]:
-                darker_L = current_frame.L[x][y]
-                lighter_L = previous_frame.L[x][y]
-            else:
-                darker_L = previous_frame.L[x][y]
-                lighter_L = current_frame.L[x][y]
-
-            if darker_L < 0.8 and lighter_L - darker_L > lighter_L * 0.1:
-                general_count = general_count + 1
-    if general_count > previous_frame.raw_array.size / 36:
-        # detected
-        return True
-    return False
-
-
-def red_transition(previous_frame, current_frame):
-    height = previous_frame.raw_array.shape[0]
-    width = previous_frame.raw_array.shape[1]
-
-    red_count = 0
-    for x in range(height):
-        for y in range(width):
-            # red opposing transition formula
-            # it's possible previous threshold isn't needed - fixme - see algorithm again
-            previous_threshold = previous_frame.R[x][y] / \
-                                 (previous_frame.R[x][y] + previous_frame.G[x][y] + previous_frame.B[x][y])
-            current_threshold = current_frame.R[x][y] / \
-                                (current_frame.R[x][y] + current_frame.G[x][y] + current_frame.B[x][y])
-
-            previous_delta = (previous_frame.R[x][y] - previous_frame.G[x][y] - previous_frame.B[x][y]) * 320
-            current_delta = (current_frame.R[x][y] - current_frame.G[x][y] - current_frame.B[x][y]) * 320
-
-            # fixme - do this following part pixel by pixel, 
-            # then see if total number of detected pixels is greater than 1/36 of total pixels
-            if previous_threshold > 0.80 and current_threshold > 0.80 and \
-                current_delta - previous_delta > 0:
-                # red transition found
-                red_count = red_count + 1
-    
-    if red_count > previous_frame.raw_array.size / 36:
-        return True
-    return False
-
-
-def process_idxs(idxs):
-    idx_iter = iter(idxs)
-    pass
